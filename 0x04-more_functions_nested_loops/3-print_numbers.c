febcaa27272f19400a9c09303166bc07312dc2e6
okiogutu <okiogutu@gmail.com>
2022-12-15 15:33:58 -0800

    ghost

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * prints all numbers from 0 to 9
+ *
+ * Return: returns void 0
+ */
+
+void print_numbers(void)
+{
+	int n;
+	for (n = 48; n < 58; n++)
+	{
+	putchar(n)
+	}
+	putchar(10)
+}
