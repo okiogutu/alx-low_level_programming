d2d9aaed5ce09fb3ee650413f06087dece5a5899
okiogutu <okiogutu@gmail.com>
2022-12-15 13:36:32 -0800

    without compiling

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - check if a letter is upper
+ * 0x: The number to be checked
+ *
+ * Return 1 for upper 0 for any else
+ */
+int _isupper(int x);
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (i);
+	}
+	return (0);
+}
