5bf54ba568b4156a5a2b55a5e6f84d8f43fab190
okiogutu <okiogutu@gmail.com>
2022-12-20 15:05:14 -0800

    success in sight

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swapping values
+ * necessary description
+ *
+ * potent  main
+ */
+void swap_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}
