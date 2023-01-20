7d900ccbaf6e217b9959bff57867ae031c3728e2
okiogutu <okiogutu@gmail.com>
2023-01-19 19:40:47 -0800

    functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+
+/**
+ * op_add - calcs sum of two integers
+ * @a: first integer
+ * @b: second int
+ *
+ * Return: sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
